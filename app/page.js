6de714c3eb2b6949e78a742e7b7f4a4bf85fc89a
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-07-25 06:00:15 +0600

    Hobbies section responsive done

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
         <AboutMe />
         <Skills />
-        {/* <Hobbies /> */}
+        <Hobbies />
         {/* <Portfolio /> */}
         {/* <Contact /> */}
     </>
--- componnets/Hobbies.jsx
+++ componnets/Hobbies.jsx
@@ -26,11 +26,11 @@ const Hobbies = () => {
     ]
 
     return (
-        <section className="relative mt-[200px]">
+        <section className="relative mt-[150px] sm:mt-[200px]">
             {/* Hobbies */}
-            <div className="flex gap-[30px]">
+            <div className="flex flex-col gap-y-[15px] sm:flex-row items-center sm:gap-[30px]">
                 {hobbies.map((hobbie, idx) => (
-                    <div key={idx} className="w-[240px] h-[195px] rounded-[4px] bg-[#1D293A] px-[25px] py-[45px]">
+                    <div key={idx} className="w-[90%] sm:w-[240px] h-[195px] rounded-[4px] bg-[#1D293A] px-[25px] py-[45px]">
                         {hobbie.icon}
                         <p className="text-white/85 text-[22px] font-black leading-6 mt-[15px]">{hobbie.name}</p>
                     </div>
@@ -38,7 +38,7 @@ const Hobbies = () => {
             </div>
 
             {/* Hobbie side text */}
-            <div className="absolute top-0 right-[-80px]">
+            <div className="absolute right-1/2 top-[-60px] translate-x-1/2 sm:translate-0 sm:top-0 sm:right-[-80px]">
                 <HobbieText />
             </div>
         </section>
